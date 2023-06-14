2701e0ee85515eb57eed519eb811f203d96d9900
DELL <lakshmi.allu04@gmail.om>
2023-06-14 17:53:59 +0530

    first shell script create

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,5 @@
 #To print a messages there are couple of commands ut echo widely used
 echo hello world
 echo -e "\e[32mhello world\e[0m"
+echo -e "\e[33mhello world\e[0m"
+echo -e "\e[34mhello world\e[0m"
